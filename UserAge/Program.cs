a55d5cc8672115492d3c87cc0db8c195276c25a1
admin-julia.voronetskaja <admin-julia.voronetskaja@tthk.ee>
2021-09-11 09:30:55 +0300

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis
+            //"oled sündinud aastal {yearOfBirth}"
+
+        }
+    }
+}
